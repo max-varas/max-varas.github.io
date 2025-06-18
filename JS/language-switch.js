2219da74fe378b7b0608b7cf8f3d76c58afff584
Max Varas H. <maxi.varas.lch@gmail.com>
2025-06-17 22:37:22 -0400

    Update language-switch.js

--- JS/language-switch.js
+++ JS/language-switch.js
@@ -1,29 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const langEnBtn = document.getElementById('EnglishButton');
     const langEsBtn = document.getElementById('SpanishButton');
-//    if (!langEnBtn||!langEsBtn) {
-//      console.warn("Language switcher buttons not found. No se han encontrado los botones de cambio de idioma.");
-//      return;
-//    }
-  let currentPath = window.location.pathname;
-  const isEnglishPage = currentPath.startsWith('/en/');
-  langEsBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    if (isEnglishPage) {
-      const newPath = currentPath.replace('/en/', '/');
-      window.location.href = window.location.origin + newPath;
+    if (!langEnBtn||!langEsBtn) {
+      console.warn("Language switcher buttons not found. No se han encontrado los botones de cambio de idioma.");
     }
-  });
-  langEnBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    if (!isEnglishPage) {
-      let newPath;
-      if (currentPath === '/' || currentPath === '/index.html' || currentPath === 'index.html') {
-        newPath = '/en/';
-      } else {
-        newPath = '/en' + currentPath;
+  if (langEsBtn) {  
+    let currentPath = window.location.pathname;
+    const isEnglishPage = currentPath.startsWith('/en/');
+    langEsBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+      if (isEnglishPage) {
+        const newPath = currentPath.replace('/en/', '/');
+        window.location.href = window.location.origin + newPath;
       }
-      window.location.href = window.location.origin + newPath;
-    }
-  });
+    });
+  } else {
+    langEnBtn.addEventListener('click', function(e){
+      e.preventDefault();
+      if (!isEnglishPage) {
+        let newPath;
+        if (currentPath === '/' || currentPath === '/index.html' || currentPath === 'index.html') {
+          newPath = '/en/';
+        } else {
+          newPath = '/en' + currentPath;
+        }
+        window.location.href = window.location.origin + newPath;
+      }
+    });
+  }
 });
