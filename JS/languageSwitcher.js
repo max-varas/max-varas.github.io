d31608a406f0f9b359bc761591b78fcb8d8624da
Max Varas H. <maxi.varas.lch@gmail.com>
2025-07-05 22:27:47 -0400

    Create languageSwitcher.js

---
+++ JS/languageSwitcher.js
@@ -0,0 +1,25 @@
+export function switchToEnglish() {
+    let currentPath = window.location.pathname;
+    const isEnglishPage = currentPath.startsWith('/en/');
+    if (!isEnglishPage) {
+        let newPath;
+        if (currentPath === '/' || currentPath === '/index.html' || currentPath === 'index.html') {
+          newPath = '/en/index.html';
+        } else {
+          newPath = '/en' + currentPath;
+        }
+        console.log("Current Path:", currentPath);
+        console.log("Calculated New Path:", newPath);
+        const fullRedirectUrl = window.location.origin + newPath;
+        console.log("Attempting to redirect to:", fullRedirectUrl);
+        window.location.href = fullRedirectUrl;
+    }
+}
+export function switchToSpanish() {
+    let currentPath = window.location.pathname;
+    const isEnglishPage = currentPath.startsWith('/en/');
+    if (isEnglishPage) {
+        const newPath = currentPath.replace('/en/', '/');
+        window.location.href = window.location.origin + newPath;
+    }
+}
